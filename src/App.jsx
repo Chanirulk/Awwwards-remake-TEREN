c08817ce142cc0c82eaae448b2caea562fed6545
chanirulk <sethumwalgama@gmail.com>
2022-06-01 15:07:56 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Home from "./components/Home/Home";
+
+
+const App = () => {
+  return (
+    <div>
+      <Home/>
+    </div>
+  );
+};
+
+export default App;
+
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,121 @@
+import "./home.css";
+import A from "../img/back.mp4";
+import B from "../img/LOGO.png";
+const Home = () => {
+    return (
+      <div className="h">
+        <img src={B} alt="" className="head" />
+        <video src={A} autoPlay muted loop className="back-vid"></video>
+        <div className="block">
+          <p className="text">teren</p>
+        </div>
+        <div className="block-two">
+          <p className="text-two">information of our people</p>
+        </div>
+        <div className="block-three">
+          <p className="text-three"><a href="">INFO</a> 	&nbsp;	&nbsp;   <a href="">Work</a> 	&nbsp;	&nbsp; 
+          <a href="">about</a> 	&nbsp; 	&nbsp; <a href="">contact</a></p>
+        </div>
+        <div className="block-four">
+          <p className="text-four">CT:90.00
+          czech islands - 0.t3f <br />
+          construction site <br />
+          all in site load - on pick <br />
+          (1241.rg54H0H) boakeas <br />
+          bobby hill </p>
+        </div>
+        <div className="block-five">
+          <p className="text-five">SZUB BERG LORRY  <br />
+          
+          <a href="" className="as">click me?</a></p>
+        </div>
+        <div className="block-six">
+          <p className="text-four">CT:90.00
+          czech islands - 0.t3f <br />
+          construction site <br />
+          all in site load - on pick <br />
+          (1241.rg54H0H) boakeas <br />
+          bobby hill </p>
+        </div>
+        <div className="block-seven">
+          <p className="text-seven">SZUB BERG LORRY  <br />
+          
+          <a href="" className="as-seven">click me?</a></p>
+        </div>
+
+
+
+        <div className="block-eight">
+          <p className="text-eight">CT:90.00
+          czech islands - 0.t3f <br />
+          construction site <br />
+          all in site load - on pick <br />
+          (1241.rg54H0H) boakeas <br />
+          bobby hill </p>
+        </div>
+        <div className="block-nine">
+          <p className="text-nine">SZUB BERG LORRY  <br />
+          
+          <a href="" className="as-nine">click me?</a></p>
+        </div>
+
+
+        
+        <div className="block-eight-2">
+          <p className="text-eight-2">CT:90.00
+          czech islands - 0.t3f <br />
+          construction site <br />
+          all in site load - on pick <br />
+          (1241.rg54H0H) boakeas <br />
+          bobby hill </p>
+        </div>
+        <div className="block-nine-2">
+          <p className="text-nine-2">SZUB BERG LORRY  <br />
+          
+          <a href="" className="as-nine-2">click me?</a></p>
+        </div>
+
+
+        
+        <div className="block-eight-3">
+          <p className="text-eight-3">CT:90.00
+          czech islands - 0.t3f <br />
+          construction site <br />
+          all in site load - on pick <br />
+          (1241.rg54H0H) boakeas <br />
+          bobby hill </p>
+        </div>
+        <div className="block-nine-3">
+          <p className="text-nine-3">SZUB 123431 laykina  2341 vre  <br />
+          
+          <a href="" className="as-nine-3">click me?</a></p>
+        </div>
+
+ 
+
+        
+        <div className="block-eight-4">
+          <p className="text-eight-4">CT:90.00
+          czech islands - 0.t3f <br />
+          construction site <br />
+          all in site load - on pick <br />
+          (1241.rg54H0H) boakeas <br />
+          bobby hill </p>
+        </div>
+        <div className="block-nine-4">
+          <p className="text-nine-4">SZUB BERG LORRY  <br />
+          
+          <a href="" className="as-nine-4">click me?</a></p>
+        </div>
+
+        <div className="foot">
+          <a href="https://www.instagram.com/chaniru.lk/" className="foot-text">Credit - @chaniru.lk</a>
+        </div>
+        <video src={A} autoPlay muted loop className="foot-vid"></video>
+      </div>
+    );
+  };
+  
+export default Home;
+  
+  
